eaa53104a58b4bc02aece02df2ee7b2aa9fb65c9
icecolla <icecoladrink@gmail.com>
2020-11-23 09:21:59 +0300

    Added game files

---
+++ Assets/Scripts/Manager/PauseManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class PauseManager : MonoBehaviour
+{
+    public AudioMixerSnapshot paused;
+    public AudioMixerSnapshot unpaused;
+
+    Canvas canvas;
+
+    private void Start()
+    {
+        canvas = GetComponent<Canvas>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            canvas.enabled = !canvas.enabled;
+            Pause();
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+        Lowpass();
+    }
+
+    void Lowpass()
+    {
+        if(Time.timeScale == 0)
+        {
+            paused.TransitionTo(.01f);
+        }
+        else
+        {
+            unpaused.TransitionTo(.01f);
+        }
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
